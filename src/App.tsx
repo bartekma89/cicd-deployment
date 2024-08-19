0a6467ff839b88b7dae55fa07b60490fa6ca796c
Bartłomiej Marzec <bmarzec@sii.pl>
2024-08-19 23:08:15 +0200

    added the deploy action

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+      <div>Practicing CI/CD</div>
       <div>
         {APP_VERSION} - {APP_TEST} - {import.meta.env.VITE_PORT}
       </div>
